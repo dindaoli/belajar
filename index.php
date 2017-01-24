ca199d6210402ecd5e883ad5cebb49e043d84347
dindaoli <dindaaml27@gmail.com>
2017-01-24 09:00:10 +0700

    nambah file profile

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
 </head>
 <body>
 	<h1>Hello world!</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus ratione hic, ut expedita saepe quam debitis aliquam magnam eveniet consectetur, iusto odit. Qui aliquid voluptates, eum culpa, quaerat doloremque ex.</p>
 </body>
 </html>
---
+++ profil.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>profile</title>
+</head>
+<body>
+	<h1>untuk kedua</h1>
+</body>
+</html>
